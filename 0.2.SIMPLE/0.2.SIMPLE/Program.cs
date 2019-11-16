5ae05bc79e855e216385b848f430966f1c39fd29
KEYtoSEE <43423808+keysee322@users.noreply.github.com>
2019-11-17 01:27:56 +0400

    ewe раз все комичу

---
+++ 0.2.SIMPLE/0.2.SIMPLE/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _0._2.SIMPLE
+{
+    class Program
+    {
+        public static void IsSimple(int n)
+        {
+            int i = 2;
+            while (i < Math.Sqrt(n) && n % i != 0) {
+                i++;
+            }
+            if (n % i ==0 && n!=2 || n==1)
+            {
+                Console.WriteLine("Number isn't simple");
+            } else
+            {
+                Console.WriteLine("Number is simple");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write number");
+            int n;
+        Start:
+            Console.Write("N = ");
+            bool success = Int32.TryParse(Console.ReadLine(), out n);
+            if (success && n > 0 && (n%1.0 == 0))
+            {
+                IsSimple(n);
+            }
+            else
+            {
+                Console.WriteLine("Try again");
+                goto Start;
+            }
+        }
+    }
+}
