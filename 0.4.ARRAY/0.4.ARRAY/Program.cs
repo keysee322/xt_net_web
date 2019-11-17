20c2386a2220ff938d064f576e15430ed52381dc
KEYtoSEE <43423808+keysee322@users.noreply.github.com>
2019-11-17 14:27:49 +0400

    Обновил 4е задание, до этого сортировка работала не по всему массиву

--- 0.4.ARRAY/0.4.ARRAY/Program.cs
+++ 0.4.ARRAY/0.4.ARRAY/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace _0._4.ARRAY
 {
@@ -38,6 +39,7 @@ namespace _0._4.ARRAY
 
         StartProgram:
             int[][] array = new int[n][];
+            List<int> numbers = new List<int> {};
             int size;
             Random rnd = new Random();
             for (int i = 0; i<n; i++)
@@ -45,14 +47,16 @@ namespace _0._4.ARRAY
                 Console.WriteLine("Input {0} array size", i);
                 size = int.Parse(Console.ReadLine());
                 array[i] = new int[size];
-                for (int j=0; j < size; j++)
+                for (int j=0; j < size; j++) // одновременно заполняется массив и значения переносятся в List, для последующей сортировки там
                 {
                     array[i][j] = rnd.Next(0, 100);
+                    numbers.Add(array[i][j]);       
                 }
+
             }
             Console.WriteLine("Source array:");
 
-            for (int i = 0; i < n; ++i)
+            for (int i = 0; i < n; ++i) 
             {
                 for (int j = 0; j < array[i].Length; ++j)
                     Console.WriteLine("\t array[{0}][{1}] = {2}", i, j, array[i][j]);
@@ -61,26 +65,40 @@ namespace _0._4.ARRAY
 
             Console.WriteLine("Sorted array:");
 
-            int temp;
-            for (int k = 0; k<n; k++)
-            for (int i = 0; i < array[k].Length - 1; i++)
+            int temp; // сортировка List
+            for (int i = 0; i < numbers.Count - 1; i++)
             {
-                for (int j = i + 1; j < array[k].Length; j++)
+                for (int j = i + 1; j < numbers.Count; j++)
                 {
-                    if (array[k][i] > array[k][j])
+                    if (numbers[i] > numbers[j])
                     {
-                        temp = array[k][i];
-                        array[k][i] = array[k][j];
-                        array[k][j] = temp;
+                        temp = numbers[i];
+                        numbers[i] = numbers[j];
+                        numbers[j] = temp;
                     }
                 }
             }
+
+
+            int k=0; //индекс по общему кол-ву эл-ов
+            for (int i = 0; i < n; i++) //Заносятся отсортированные значения из List в массив
+            {
+                for (int j = 0; j < array[i].Length; j++)
+                {   
+                    array[i][j] = numbers[k];
+                    k++;
+                }
+
+            }
+
+
             for (int i = 0; i < n; ++i)
             {
                 for (int j = 0; j < array[i].Length; ++j)
                     Console.WriteLine("\t array[{0}][{1}] = {2}", i, j, array[i][j]);
                 Console.WriteLine();
             }
+            Console.WriteLine(array.Length);
         }
     }
 }
