4129b2654c420836683981a73b8fd36785698ccb
KEYtoSEE <43423808+keysee322@users.noreply.github.com>
2019-11-19 22:40:49 +0400

    1. Прямоугольник

---
+++ 1_lab/1.1. RECTANGLE/1.1. RECTANGLE/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _1._1._RECTANGLE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write number");
+            int a,b;
+        Start:
+            Console.Write("a = ");
+
+            bool success = Int32.TryParse(Console.ReadLine(), out a);
+            Console.Write("b = ");
+            bool success1 = Int32.TryParse(Console.ReadLine(), out b);
+            if (success && success1 && a > 0 && b>0)
+            {
+                Console.WriteLine("S = {0}", a * b);
+            }
+            else
+            {
+                Console.WriteLine("Try again");
+                goto Start;
+            }
+        }
+    }
+}
+
