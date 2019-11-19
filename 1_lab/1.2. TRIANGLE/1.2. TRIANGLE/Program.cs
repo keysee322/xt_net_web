45d65d5b2658ce50adf8a2d237189b683871df54
KEYtoSEE <43423808+keysee322@users.noreply.github.com>
2019-11-19 22:50:33 +0400

    2- Половина ёлки

---
+++ 1_lab/1.2. TRIANGLE/1.2. TRIANGLE/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _1._2._TRIANGLE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write number");
+            int n;
+        Start:
+            Console.Write("N = ");
+            bool success = Int32.TryParse(Console.ReadLine(), out n);
+            if (success && n > 0 && (n % 1.0 == 0))
+            {
+                for (int i = 1; i<=n; i++)
+                {
+                    for (int j = 1; j <= i; j++)
+                        Console.Write("*");
+                    Console.WriteLine();
+                }
+                
+            }
+            else
+            {
+                Console.WriteLine("Try again");
+                goto Start;
+            }
+        }
+    }
+}
