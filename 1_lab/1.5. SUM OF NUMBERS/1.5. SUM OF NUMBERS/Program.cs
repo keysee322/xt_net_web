1ef4644197cce3c538c95c90b7e383c6b0549821
KEYtoSEE <43423808+keysee322@users.noreply.github.com>
2019-11-20 02:51:25 +0400

    Добавил пятое задание

---
+++ 1_lab/1.5. SUM OF NUMBERS/1.5. SUM OF NUMBERS/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _1._5._SUM_OF_NUMBERS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { int n=0;
+            for (int i = 0; i < 1000; i++)
+                if (i % 5 == 0 || i % 3 == 0)
+                    n += i;
+            Console.WriteLine("{0}", n);
+        }
+    }
+}
