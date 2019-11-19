1a8f27edd7e79d02c3bd6787d581867510947065
KEYtoSEE <43423808+keysee322@users.noreply.github.com>
2019-11-20 02:25:17 +0400

    Добавил третье задание

---
+++ 1_lab/1.3. Another Triangle/1.3. Another Triangle/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _1._3._Another_Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write number");
+            int n;
+        Start:
+            Console.Write("N = ");
+            bool success = Int32.TryParse(Console.ReadLine(), out n);
+            if (success && n > 0 && (n % 1.0 == 0))
+            {
+                for (int i = 1; i <= n; i++)
+                {
+                    for (int j = 1; j < n*2; j++)
+                        if (j <= n-i || j>=n+i)
+                            Console.Write(" ");
+                    else
+                    Console.Write("*");
+                    Console.WriteLine();
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Try again");
+                goto Start;
+            }
+        }
+    }
+}
