705167a24a58b86fc8b159c4cf3a3ad2dfd4cce2
KEYtoSEE <43423808+keysee322@users.noreply.github.com>
2019-11-20 13:52:41 +0400

    Добавил четвёртое задание

---
+++ 1_lab/1.4. X-MAS TREE/1.4. X-MAS TREE/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _1._4._X_MAS_TREE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Write number");
+            int n;
+        Start:
+            Console.Write("N = ");
+            bool success = Int32.TryParse(Console.ReadLine(), out n);
+            if (success && n > 0 && (n % 1.0 == 0))
+            {for (int k=1;k<=n;k++)
+                for (int i = 1; i <= k; i++)
+                {
+                    for (int j = 1; j < n * 2; j++)
+                        if (j <= n - i || j >= n + i)
+                            Console.Write(" ");
+                        else
+                            Console.Write("*");
+                    Console.WriteLine();
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Try again");
+                goto Start;
+            }
+        }
+    }
+}
+
